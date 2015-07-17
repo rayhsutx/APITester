109d52982f5e03e8f6e58d9a869dd26c16b365f6
Ray Hsu <rs0939@gmail.com>
2015-07-17 15:48:17 +0800

    print out version

--- src/com/kaisquare/kainode/tester/KaiNodeTester.java
+++ src/com/kaisquare/kainode/tester/KaiNodeTester.java
@@ -16,6 +16,7 @@ public class KaiNodeTester {
 	public static final String VERSION = "1.0";	
 
 	public static void main(String[] args) {
+		System.out.println("version " + VERSION);
 		if (!Files.exists(Paths.get("log4j.properties")))
 		{
 			URL configURL = KaiNodeTester.class.getResource("/res/log4j.properties");
